962c6a2f9d1d2a00a97fd30d1836855b30eb8cd2
Prashant Singh <singh08prashant@gmail.com>
2020-10-01 15:10:47 +0530

    create homescreen route

--- lib/theme/routes.dart
+++ lib/theme/routes.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:book_donation/views/login_screen.dart';
 import 'package:book_donation/views/register_screen.dart';
+import 'package:book_donation/views/home_screen.dart';
 
 class AppRoutes {
   AppRoutes._();
 
   static const String authLogin = '/auth-login';
   static const String authRegister = '/auth-register';
+  static const String homeScreen = '/home-screen';
 
   static Map<String, WidgetBuilder> define(){
     return{
     authLogin: (context) => Login(),
     authRegister: (context) => Register(),
+    homeScreen: (context) => Home(),
   };
 }
 }
---
+++ lib/views/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:book_donation/theme/routes.dart';
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
