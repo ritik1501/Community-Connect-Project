52ea8fce578a28ee8f7819810a99b4b445c3048b
Prashant Singh <singh08prashant@gmail.com>
2020-11-07 18:00:14 +0530

    Update routes.dart

--- lib/theme/routes.dart
+++ lib/theme/routes.dart
@@ -1,3 +1,4 @@
+import 'package:book_donation/views/opening_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:book_donation/views/login_screen.dart';
 import 'package:book_donation/views/register_screen.dart';
@@ -9,12 +10,14 @@ class AppRoutes {
   static const String authLogin = '/auth-login';
   static const String authRegister = '/auth-register';
   static const String homeScreen = '/home-screen';
+  static const String openingScreen = '/opening-screen';
 
   static Map<String, WidgetBuilder> define(){
     return{
     authLogin: (context) => Login(),
     authRegister: (context) => Register(),
     homeScreen: (context) => Home(),
+    openingScreen: (context) => OpeningView(),
   };
 }
 }
